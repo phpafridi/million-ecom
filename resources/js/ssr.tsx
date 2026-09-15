40ad0316206f7c441e6182e25d0fd993d71d5156
afridi <phpafridi@gmail.com>
2026-09-15 11:52:26 +0500

    2.4

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,18 +1,19 @@
 import ReactDOMServer from 'react-dom/server'
 import { createInertiaApp } from '@inertiajs/react'
+import createServer from '@inertiajs/react/server'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
-export default function render(page: any) {
-    return createInertiaApp({
+createServer((page) =>
+    createInertiaApp({
         page,
-        render: ReactDOMServer.renderToString,
         title: (title) => title
             ? `${title} — MILLIONAIRE`
             : 'MILLIONAIRE — Wear Your Status',
+        render: ReactDOMServer.renderToString,
         resolve: (name) => resolvePageComponent(
             `./Pages/${name}.tsx`,
             import.meta.glob('./Pages/**/*.tsx'),
         ),
         setup: ({ App, props }) => <App {...props} />,
-    })
-}
+    }),
+)
